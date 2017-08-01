92a7c3fa00edf74bead21059569e7f338410c273
nozuonodieingdouble <1532511761@qq.com>
2017-08-01 16:59:32 +0800

    饿汉模式

---
+++ 单例模式/饿汉模式.cpp
@@ -0,0 +1,17 @@
+//¶öººÄ£Ê½
+class singleton
+{
+protected:
+	singleton()
+	{}
+private:
+	static singleton* p;
+public:
+	static singleton* initance();
+};
+singleton* singleton::p = new singleton;
+
+singleton* singleton::initance()
+{
+	return p;
+}
